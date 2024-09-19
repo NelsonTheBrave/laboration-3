034c005f9b8cbb85882a53c4a9811b0534b47577
Nelson <sherman.erik@gmail.com>
2024-09-19 14:54:12 +0200

    Added tests to all public methods in Warehouse (except main())

--- src/main/java/service/Warehouse.java
+++ src/main/java/service/Warehouse.java
@@ -16,89 +16,45 @@ public class Warehouse {
     }
 
     public void addProduct(String name, Category category, int rating) {
-        // Add a new product to the warehouse
+
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Product name cannot be null or empty");
+        }
         Product product = new Product(products.size() + 1, name, category, rating);
         products.add(product);
     }
 
-    public void printProducts() {
-        // Print all products in the warehouse
-        for (Product product : products) {
-            System.out.println("Id: " + product.id + ", Product: " + product.name + ", Category: " + product.category + ", Rating: " + product.rating + ", Manufactured: " + product.manufactureDate + ", Last Modified: " + product.lastModifiedDate);
-        }
-    }
-
     public void modifyProduct(int id, String newName, Category newCategory, int newRating) {
         // Modify an existing product in the warehouse
-        products.stream()
-                .filter(product -> product.id == id)
-                .findFirst()
-                .ifPresent(product -> {
-                    product.name = newName;
-                    product.category = newCategory;
-                    product.rating = newRating;
-                    product.setLastModifiedDate(LocalDateTime.now());
-                });
+        products.stream().filter(product -> product.id == id).findFirst().ifPresent(product -> {
+            product.name = newName;
+            product.category = newCategory;
+            product.rating = newRating;
+            product.setLastModifiedDate(LocalDateTime.now());
+        });
     }
 
     public List<Product.ProductRecord> getAllProducts() {
-        return products.stream()
-                .map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate))
-                .collect(Collectors.toList());
+        return products.stream().map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate)).collect(Collectors.toList());
     }
 
     public Product.ProductRecord getProductById(int id) {
-        return products.stream()
-                .filter(product -> product.id == id)
-                .map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate))
-                .findFirst()
-                .orElse(null);
+        return products.stream().filter(product -> product.id == id).map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate)).findFirst().orElse(null);
     }
 
     public List<Product.ProductRecord> getProductsByCategory(Category category) {
-        return products.stream()
-                .filter(product -> product.category == category)
-                .sorted(Comparator.comparing(p -> p.name))
-                .map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate))
-                .collect(Collectors.toList());
+        return products.stream().filter(product -> product.category == category).sorted(Comparator.comparing(p -> p.name)).map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate)).collect(Collectors.toList());
     }
 
     public List<Product.ProductRecord> getProductsManufacturedAfterDate(LocalDateTime date) {
-        return products.stream()
-                .filter(product -> product.manufactureDate.isAfter(date))
-                .sorted(Comparator.comparing((Product product) -> product.manufactureDate).reversed())
-                .map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate))
-                .collect(Collectors.toList());
+        return products.stream().filter(product -> product.manufactureDate.isAfter(date)).sorted(Comparator.comparing((Product product) -> product.manufactureDate).reversed()).map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate)).collect(Collectors.toList());
     }
 
     public List<Product.ProductRecord> getAllModifiedProducts() {
-        return products.stream()
-                .filter(product -> product.manufactureDate.isBefore(product.lastModifiedDate))
-                .map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate))
-                .collect(Collectors.toList());
+        return products.stream().filter(product -> product.manufactureDate.isBefore(product.lastModifiedDate)).map(product -> new Product.ProductRecord(product.id, product.name, product.category, product.rating, product.manufactureDate, product.lastModifiedDate)).collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
-        Warehouse warehouse = new Warehouse();
-        warehouse.addProduct("Apple", Category.FOOD, 5);
-        warehouse.addProduct("Banana", Category.FOOD, 2);
-        warehouse.addProduct("Shirt", Category.CLOTHES, 3);
-        warehouse.addProduct("Sweater", Category.CLOTHES, 3);
-        warehouse.addProduct("Blouse", Category.CLOTHES, 3);
 
-        warehouse.modifyProduct(2, "Orange", Category.FOOD, 4);
-        warehouse.modifyProduct(5, "Vodka", Category.DRINK, 1);
-
-        warehouse.printProducts();
-
-//        warehouse.getAllProducts().forEach(System.out::println);
-
-//        System.out.println(warehouse.getProductById(3));
-
-//        warehouse.getProductsByCategory(Category.CLOTHES).forEach(System.out::println);
-        
-//        warehouse.getProductsManufacturedAfterDate(LocalDateTime.now().minusDays(1)).forEach(System.out::println);
-        warehouse.getAllModifiedProducts().forEach(System.out::println);
     }
 }
---
+++ src/test/java/service/WarehouseTest.java
@@ -0,0 +1,133 @@
+package service;
+
+import entities.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WarehouseTest {
+
+    private Warehouse warehouse;
+
+    @BeforeEach
+    void setUp() {
+        warehouse = new Warehouse();
+        warehouse.addProduct("Apple", Category.FOOD, 5);
+        warehouse.addProduct("Banana", Category.FOOD, 2);
+    }
+
+    void setUpSingleProduct() {
+        warehouse = new Warehouse();
+        warehouse.addProduct("Apple", Category.FOOD, 5);
+    }
+
+    @Test
+    @DisplayName("Adding a product to the warehouse")
+    void testAddProduct() {
+        setUpSingleProduct();
+
+        assertThat(warehouse.products)
+                .hasSize(1)
+                .first()
+                .matches(product -> product.name.equals("Apple"), "Product name is Apple")
+                .matches(product -> product.category == Category.FOOD, "Product category is FOOD");
+    }
+
+    @Test
+    @DisplayName("Adding a product with null or an empty name string should throw an exception")
+    void testAddProductFailure() {
+
+        assertThatThrownBy(() -> warehouse.addProduct(null, Category.FOOD, 5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Product name cannot be null or empty");
+
+        assertThatThrownBy(() -> warehouse.addProduct("", Category.FOOD, 5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Product name cannot be null or empty");
+    }
+
+    @Test
+    @DisplayName("Modifying a product in the warehouse")
+    void testModifyProduct() {
+        warehouse.modifyProduct(1, "Orange", Category.FOOD, 3);
+
+        assertThat(warehouse.products)
+                .hasSize(2)
+                .extracting("name", "category", "rating")
+                .contains(
+                        tuple("Orange", Category.FOOD, 3),
+                        tuple("Banana", Category.FOOD, 2)
+                );
+    }
+
+    @Test
+    @DisplayName("Get all products in the warehouse")
+    void testGetAllProducts() {
+
+        assertThat(warehouse.getAllProducts())
+                .hasSize(2)
+                .extracting("name", "category", "rating")
+                .contains(
+                        tuple("Apple", Category.FOOD, 5),
+                        tuple("Banana", Category.FOOD, 2)
+                );
+    }
+
+    @Test
+    @DisplayName("Get a product by id")
+    void testGetProductById() {
+
+        assertThat(warehouse.getProductById(2))
+                .extracting("name", "category", "rating")
+                .contains("Banana", Category.FOOD, 2);
+    }
+
+    @Test
+    @DisplayName("Get products by category")
+    void testGetProductsByCategory() {
+        warehouse.addProduct("Shirt", Category.CLOTHES, 3);
+
+        assertThat(warehouse.getProductsByCategory(Category.FOOD))
+                .hasSize(2)
+                .extracting("name", "category")
+                .contains(
+                        tuple("Apple", Category.FOOD),
+                        tuple("Banana", Category.FOOD)
+                );
+    }
+
+    @Test
+    @DisplayName("Get all modified products")
+    void testGetAllModifiedProducts() throws InterruptedException {
+        Thread.sleep(100);
+        warehouse.modifyProduct(1, "Orange", Category.FOOD, 3);
+
+        warehouse.getAllProducts().forEach(System.out::println);
+        assertThat(warehouse.getAllModifiedProducts())
+                .hasSize(1)
+                .extracting("name", "category", "rating")
+                .contains(
+                        tuple("Orange", Category.FOOD, 3)
+                );
+    }
+
+    @Test
+    @DisplayName("Get products manufactured after a certain date")
+    void testGetProductsManufacturedAfterDate() {
+
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+        assertThat(warehouse.getProductsManufacturedAfterDate(yesterday))
+                .hasSize(2)
+                .extracting("name", "category")
+                .contains(
+                        tuple("Apple", Category.FOOD),
+                        tuple("Banana", Category.FOOD)
+                );
+    }
+}
+
